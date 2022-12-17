0704b248400eefe446f9f18eeb549036c492edd2
Serg0857 <119527957+Serg0857@users.noreply.github.com>
2022-12-17 02:14:07 +0200

    from lesson 28

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,77 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //System.out.println("Hello Sergey");
+        /*int my_number = 5;
+        double second_number;
+
+        System.out.println( "My number is "  + my_number);
+        second_number = 22.5;
+
+        System.out.println( "My number is "  + second_number);
+
+        String my_string = "my first sting";
+
+        System.out.println (my_string);
+        System.out.print (my_string.toLowerCase());
+        System.out.print (my_string.toUpperCase());
+        System.out.print (my_string.length());
+        System.out.print (my_string.charAt(0));*/
+
+        /*int a = 10;
+        int b = 3;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);*/
+
+        /*int a = 10;
+        int b = 3;*/
+
+        /*System.out.println(++a); // сначала +1 потом показать
+        System.out.println(--b);// сначала -1 потом показать
+
+        System.out.println(a++);// сначала показать потом +1
+        System.out.println(b--);// сначала показать потом -1
+
+        System.out.println(a);
+        System.out.println(b);*/
+
+        // != значение не равно? , == равно ли два значения?
+
+
+        /*System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);*/
+
+
+        /*int currentYear = 2022;
+        int  friendYear = 1990;
+
+        System.out.println(currentYear-friendYear);
+
+        System.out.println( "My friend is "  + (currentYear-friendYear) + " years old");*/
+
+        /*int number = 12421;
+
+        String lastDigit = "12421";
+        System.out.println(lastDigit.charAt(4));
+        System.out.println( "lastDigit = "  + lastDigit.charAt(4));*/
+
+
+        int age = 18 ;
+
+
+        if (age<=30) {
+            System.out.println( " Michael, congratulations you know how works if");
+        }
+        else {
+            System.out.println( " Michael, congratulations you know how works if");
+        }
     }
 }
